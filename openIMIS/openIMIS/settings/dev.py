2d1d08423f13cb77ae397bfaa05b0e18c1e1f4a2
David Pella <daviejaco@gmail.com>
2025-11-06 16:53:44 +0300

    WIP

--- openIMIS/openIMIS/settings/dev.py
+++ openIMIS/openIMIS/settings/dev.py
@@ -3,12 +3,16 @@ import os
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Set ALLOWED_HOSTS
 ALLOWED_HOSTS = ['*']
+
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost',
+    'https://localhost',
     'http://192.168.0.1',
     'http://localhost:8000',
+    'https://localhost:8000',
     'http://192.168.0.1:8000',
     'http://localhost:3000',
+    'https://localhost:3000',
     'http://192.168.0.1:3000',
 ]
 # Set CORS_ALLOWED_ORIGINS to match CSRF_TRUSTED_ORIGINS
